8d0de14d918537048814ed0e8711feacb7a5d817
ClearSky0 <58333103+ClearSky0@users.noreply.github.com>
2022-07-20 09:56:38 +0100

    After adding publication to models and page.

--- blog/models.py
+++ blog/models.py
@@ -2,9 +2,22 @@ from django.conf import settings
 from django.db import models
 from django.utils import timezone
 
+class Tag(models.Model):
+    name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.name
+
+class Publication(models.Model):
+    name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.name
 
 class Post(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    tags = models.ManyToManyField(Tag)
+    publication = models.ForeignKey(Publication, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     text = models.TextField()
     created_date = models.DateTimeField(default=timezone.now)

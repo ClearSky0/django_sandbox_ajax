3254dba26f34354490bf108b30fb6eebf4432ed0
ClearSky0 <58333103+ClearSky0@users.noreply.github.com>
2022-07-20 20:28:42 +0100

    Put the tags back.

--- blog/forms.py
+++ blog/forms.py
@@ -6,4 +6,4 @@ class PostForm(forms.ModelForm):
 
     class Meta:
         model = Post
-        fields = ('publication', 'title', 'text',)
+        fields = ('publication', 'title', 'tags', 'text',)
